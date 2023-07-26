c18e51f1b9c6c30937d474516a62e459ca20bc15
Gabriel RF <github@gabrf.com>
2023-07-26 19:28:36 -0300

    v0

--- Voice2Text.py
+++ Voice2Text.py
@@ -4,8 +4,8 @@ import time
 import os
 import whisper
 
-api_id = os.environ.get('api_id')
-api_hash = os.environ.get(f'api_hash')
+api_id = 
+api_hash = ""
 
 app = Client("Voice2Text", api_id=api_id, api_hash=api_hash)
 model = whisper.load_model("base")
@@ -17,7 +17,11 @@ def voice_to_text(voice_file, i=0):
     if i == 5:
         return False
     try:
-        result = model.transcribe(voice_file, fp16=False, language='Portuguese')['text']
+        result = model.transcribe(
+            voice_file,
+            fp16=False,
+            language='Portuguese'
+        )['text']
     except:
         time.sleep(3)
         result = voice_to_text(voice_file, i+1)
@@ -26,27 +30,44 @@ def voice_to_text(voice_file, i=0):
 def download_voice(message):
     return app.download_media(message)
 
-def send_message(client, message, transcription):
+def edit_message(client, msg, message, transcription):
     text = (
         f'<b>🎙 Transcrição automática:</b>'
         f'\n<i>{transcription}</i>'
     )
-    client.send_message(
+    client.edit_message_text(
         chat_id=message.chat.id,
+        message_id=msg.id,
         text=text,
-        reply_to_message_id=message.id,
         parse_mode=enums.ParseMode.HTML
+    )
+
+def send_message(client, message):
+    msg = app.send_message(
+        chat_id=message.chat.id,
+        text='📝 Transcrição iniciada...',
+        parse_mode=enums.ParseMode.HTML,
+        reply_to_message_id=message.id
+    )
+    return msg
+
+def delete_message(client, msg):
+    app.delete_messages(
+        chat_id=msg.chat.id,
+        message_ids=msg.id
     )
 
 @app.on_message(filters.private & (filters.voice | filters.video_note))
 def receive_voice(client, message):
+    msg = send_message(client, message)
     app.send_chat_action(message.chat.id, enums.ChatAction.TYPING)
     voice_file = download_voice(message)
     transcription = voice_to_text(voice_file)
     if not transcription:
+        delete_message(client, msg)
         return
     print(f'{message.chat.id} disse: {transcription}')
-    send_message(client, message, transcription)
+    edit_message(client, msg, message, transcription)
     remove_file(voice_file)
 
 app.run()
